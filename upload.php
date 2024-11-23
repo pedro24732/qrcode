a4d2ddd8905feefe4eed7a721fbb77b82d371e22
pedro24732 <112995803+pedro24732@users.noreply.github.com>
2024-11-23 18:56:25 -0300

    Update upload.php
    added of site

--- upload.php
+++ upload.php
@@ -3,6 +3,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $targetDir = "uploads/";
     $targetFile = $targetDir . basename($_FILES["file"]["name"]);
     move_uploaded_file($_FILES["file"]["tmp_name"], $targetFile);
-    echo "http://seusite.com/" . $targetFile; // Retorna a URL do arquivo
+    echo "https://pedro24732.github.io/qrcode/" . $targetFile; // Retorna a URL do arquivo
 }
 ?>
